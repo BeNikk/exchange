ccb17f9d7e21599ce5826e80a8a8eecd55544b6e
BeNikk <bhattnik442@gmail.com>
2025-09-07 18:08:17 +0530

    add:engine subscribing to publisher

--- engine/src/index.ts
+++ engine/src/index.ts
@@ -1,1 +1,19 @@
-``
+import { createClient } from "redis";
+
+const subscriber = createClient();
+
+const PRICES: Record<string, { price: number; decimal: number }> = {};
+
+async function main(){
+  await subscriber.connect();
+  console.log("connected");
+  subscriber.subscribe("price_feed",(data)=>{
+    const parsedData = JSON.parse(data);
+    console.log(parsedData);
+    parsedData.price_updates.forEach(({ asset, price, decimal }: { asset: string; price: number; decimal: number }) => {
+      PRICES[asset] = { price, decimal };
+    });
+    console.log(PRICES);
+  })
+}
+main();
